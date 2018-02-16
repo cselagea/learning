55d61299c9e94d94b90c61791e9a1e849f3e358d
Cosmin <cosmin.sel@gmail.com>
2018-02-15 23:07:51 -0500

    more examples of lambdas

--- java/src/main/java/csp/learning/java/lambdas/Lambdas.java
+++ java/src/main/java/csp/learning/java/lambdas/Lambdas.java
@@ -1,10 +1,33 @@
 package csp.learning.java.lambdas;
 
+import csp.learning.java.lambdas.model.Album;
+import csp.learning.java.lambdas.model.Artist;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class Lambdas {
 
     public static final ThreadLocal<DateFormat> threadLocalDateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd-MMM-yyyy"));
+
+    public static int addUp(Stream<Integer> numbers) {
+        return numbers.mapToInt(number -> number)
+                      .sum();
+    }
+
+    public static Map<String, String> byOrigin(Stream<Artist> artists) {
+        return artists.collect(toMap(Artist::getName, Artist::getOrigin));
+    }
+
+    public static List<Album> filterIfContainsAtMostThreeTracks(Stream<Album> albums) {
+        return albums.filter(album -> album.getTracks().count() <= 3)
+                     .collect(toList());
+    }
 
 }
--- java/src/test/java/csp/learning/java/lambdas/LambdasTest.java
+++ java/src/test/java/csp/learning/java/lambdas/LambdasTest.java
@@ -1,23 +1,36 @@
 package csp.learning.java.lambdas;
 
+import csp.learning.java.lambdas.model.Album;
+import csp.learning.java.lambdas.model.Artist;
+import csp.learning.java.lambdas.model.Track;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static csp.learning.java.lambdas.Lambdas.*;
 import static java.util.Calendar.AUGUST;
 import static java.util.Calendar.DECEMBER;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 class LambdasTest {
+
+    private final Artist johnMayer = Artist.named("John Mayer").from("Bridgeport");
+    private final Artist fooFighters = Artist.named("Foo Fighters").from("Seattle");
+    private final Artist theShins = Artist.named("The Shins").from("Albuquerque");
+    private final Artist theTallestManOnEarth = Artist.named("The Tallest Man on Earth").from("Dalarna");
 
     @ParameterizedTest(name = "[{index}] {0} -> {1}")
     @MethodSource("dates")
     void testDateFormatting(Date date, String expectedFormat) {
-        String formattedDate = Lambdas.threadLocalDateFormat.get().format(date);
+        String formattedDate = threadLocalDateFormat.get().format(date);
         assertThat(formattedDate).isEqualTo(expectedFormat);
     }
 
@@ -36,6 +49,63 @@ class LambdasTest {
 
     private static Date getDate(int dayOfMonth, int month, int year) {
         return new GregorianCalendar(year, month, dayOfMonth).getTime();
+    }
+
+    @ParameterizedTest(name = "[{index}] sum = {1}")
+    @MethodSource("numbers")
+    void testAddUp(Stream<Integer> numbers, int expected) {
+        int sum = addUp(numbers);
+        assertThat(sum).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> numbers() {
+        return Stream.of(
+                Arguments.of(
+                        Stream.empty(),
+                        0
+                ),
+                Arguments.of(
+                        Stream.of(1, 2, 3),
+                        6
+                ),
+                Arguments.of(
+                        Stream.of(-2, 10, 3),
+                        11
+                )
+        );
+    }
+
+    @Test
+    void testOrganizationOfArtistsByOrigin() {
+        Stream<Artist> artists = Stream.of(johnMayer, fooFighters, theShins, theTallestManOnEarth);
+        Map<String, String> artistNameToOrigin = byOrigin(artists);
+
+        assertThat(artistNameToOrigin).contains(
+                entry("John Mayer", "Bridgeport"),
+                entry("Foo Fighters", "Seattle"),
+                entry("The Shins", "Albuquerque"),
+                entry("The Tallest Man on Earth", "Dalarna")
+        );
+    }
+
+    @Test
+    void excludeAlbumsWithMoreThanThreeTracks() {
+        Album wastingLight = Album.named("Wasting Light")
+                                  .by(fooFighters)
+                                  .with(Track.named("These Days"));
+
+        Album continuum = Album.named("Continuum")
+                               .by(johnMayer)
+                               .with(Track.named("Belief"))
+                               .with(Track.named("Gravity"))
+                               .with(Track.named("Vultures"))
+                               .with(Track.named("In Repair"));
+
+        Album chutesTooNarrow = Album.named("Chutes Too Narrow");
+
+        Stream<Album> albums = Stream.of(wastingLight, continuum, chutesTooNarrow);
+        List<Album> filtered = filterIfContainsAtMostThreeTracks(albums);
+        assertThat(filtered).contains(wastingLight, chutesTooNarrow);
     }
 
 }
