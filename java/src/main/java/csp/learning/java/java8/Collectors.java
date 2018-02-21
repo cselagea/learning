1ddaae0438c8833e00bbf65f6b0685690e43b478
Cosmin <cosmin.sel@gmail.com>
2018-02-20 22:32:45 -0500

    more examples of using collectors

--- java/src/main/java/csp/learning/java/java8/Collectors.java
+++ java/src/main/java/csp/learning/java/java8/Collectors.java
@@ -5,10 +5,7 @@ import csp.learning.java.java8.model.Artist;
 import csp.learning.java.java8.model.Track;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.util.stream.Collector;
 import java.util.stream.Stream;
 
@@ -57,6 +54,32 @@ public class Collectors {
                              ));
     }
 
+    public static Optional<Artist> longestNameByCollecting(List<Artist> artists) {
+        Function<Artist, Integer> nameLength = artist -> artist.getName().length();
+
+        return artists.stream()
+                      .filter(Objects::nonNull)
+                      .max(comparing(nameLength));
+    }
+
+    public static Optional<Artist> longestNamebyReduce(List<Artist> artists) {
+        return artists.stream()
+                      .filter(Objects::nonNull)
+                      .reduce((artist1, artist2) -> artist1.getName().length() > artist2.getName().length() ? artist1 : artist2);
+    }
+
+    public static Optional<Artist> longestNameByReduceAlternative(List<Artist> artists) {
+        Comparator<Artist> nameLength = comparing(artist -> artist.getName().length());
+
+        return artists.stream()
+                      .filter(Objects::nonNull)
+                      .reduce((artist1, artist2) -> nameLength.compare(artist1, artist2) >= 0 ? artist1 : artist2);
+    }
+
+    public static Map<String, Long> frequency(Stream<String> strings) {
+        return strings.collect(groupingBy(Function.identity(), counting()));
+    }
+
     public static class StringConcatenator implements Collector<String, StringBuilder, String> {
 
         @Override
@@ -84,6 +107,58 @@ public class Collectors {
             return Collections.emptySet();
         }
 
+    }
+
+    public static class GroupingBy<T, K> implements Collector<T, Map<K, List<T>>, Map<K, List<T>>> {
+
+        private static final Set<Characteristics> CHARACTERISTICS = new HashSet<>();
+
+        static {
+            CHARACTERISTICS.add(Characteristics.IDENTITY_FINISH);
+        }
+
+        private final Function<? super T, ? extends K> classifier;
+
+        public GroupingBy(Function<? super T, ? extends K> classifier) {
+            this.classifier = classifier;
+        }
+
+        @Override
+        public Supplier<Map<K, List<T>>> supplier() {
+            return HashMap::new;
+        }
+
+        @Override
+        public BiConsumer<Map<K, List<T>>, T> accumulator() {
+            return (map, element) -> {
+                K key = classifier.apply(element);
+                List<T> value = map.computeIfAbsent(key, k -> new ArrayList<>());
+                value.add(element);
+            };
+        }
+
+        @Override
+        public BinaryOperator<Map<K, List<T>>> combiner() {
+            BiFunction<List<T>, List<T>, List<T>> mergeLists = (list1, list2) -> {
+                list1.addAll(list2);
+                return list1;
+            };
+
+            return (map1, map2) -> {
+                map2.forEach((key, value) -> map1.merge(key, value, mergeLists));
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<K, List<T>>, Map<K, List<T>>> finisher() {
+            return Function.identity();
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return CHARACTERISTICS;
+        }
     }
 
 }
--- java/src/test/java/csp/learning/java/java8/CollectorsTest.java
+++ java/src/test/java/csp/learning/java/java8/CollectorsTest.java
@@ -1,11 +1,19 @@
 package csp.learning.java.java8;
 
 import csp.learning.java.java8.Collectors.StringConcatenator;
+import csp.learning.java.java8.model.Artist;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
+import static csp.learning.java.java8.Collectors.frequency;
+import static csp.learning.java.java8.Collectors.longestNameByCollecting;
+import static csp.learning.java.java8.model.MusicTestData.*;
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 class CollectorsTest {
 
@@ -14,6 +22,24 @@ class CollectorsTest {
         Stream<String> strings = Stream.of("first", "second", "third");
         String result = strings.collect(new StringConcatenator());
         assertThat(result).isEqualTo("first" + "second" + "third");
+    }
+
+    @Test
+    void testLongestNameByCollecting() {
+        Stream<Artist> artists = Stream.of(johnMayer, fooFighters, theShins, theTallestManOnEarth);
+        Optional<Artist> artist = longestNameByCollecting(artists.collect(toList()));
+        assertThat(artist).contains(theTallestManOnEarth);
+    }
+
+    @Test
+    void testFrequency() {
+        Stream<String> names = Stream.of("John", "Paul", "George", "John", "Paul", "John");
+        Map<String, Long> frequencyOfNames = frequency(names);
+        assertThat(frequencyOfNames).contains(
+                entry("John", 3L),
+                entry("Paul", 2L),
+                entry("George", 1L)
+        );
     }
 
 }
